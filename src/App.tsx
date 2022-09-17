02fd84484c4c2c1ed5a6b5a985f6056e338fe794
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-09-17 17:09:10 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Home} from "./pages/Home/components/Home";
 import {MonthStatistic} from "./pages/MonthStatistic/MonthStatistic";
 import {Route, Routes} from "react-router";
+import {Header} from "./shared/Header/Header";
 
 
 function App() {
     return (
-        <div className="App">
+        <div className="container">
+            <Header />
             <Routes>
                 <Route path="/"  element={<Home/>}/>
                 <Route path='/month-statistic'  element={<MonthStatistic/>}/>
---
+++ src/shared/Header/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Select from 'react-select';
+import s from './Header.module.scss';
+import {GlobalSvgSelector} from "../../assets/icons/global/GlobalSvgSelector";
+interface Props {
+
+}
+
+export const Header = (props: Props) => {
+    const options = [
+        { value: 'city-1', label: 'Гомель' },
+        { value: 'city-2', label: 'Минск' },
+        { value: 'city-3', label: 'Корма' }
+    ];
+    const colourStyles = {
+        control: (styles: any) => ({
+            ...styles,
+            backgroundColor: 'rgba(71, 147, 255, 0.2)',
+            width: '194px',
+            height: '37px',
+            border: 'none',
+            borderRadius: '10px',
+            zIndex: 100,
+        })
+    };
+    return (
+        <header className={s.header}>
+            <div className={s.wrapper}>
+                <div className={s.logo}>
+                    <GlobalSvgSelector id='header-logo'/>
+                </div>
+                <div className={s.title}>Weather</div>
+            </div>
+            <div className={s.wrapper}>
+                <div className={s.change_theme}>
+                    <GlobalSvgSelector id='change-theme'/>
+                </div>
+                <Select defaultValue={options[0]} styles={colourStyles} options={options} />
+            </div>
+        </header>
+    );
+};
