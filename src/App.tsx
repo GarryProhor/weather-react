135fb490cc92ddea6e2f2f99017b452997e8457f
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-09-17 15:59:30 +0300

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {Home} from "./pages/Home/components/Home";
+import {MonthStatistic} from "./pages/MonthStatistic/MonthStatistic";
+import {Route, Routes} from "react-router";
 
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/"  element={<Home/>}/>
+                <Route path='/month-statistic'  element={<MonthStatistic/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
