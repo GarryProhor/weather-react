e344212c75bc4ceecb74d1f8c9a57e6ac3efb530
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-09-17 19:20:51 +0300

    ThisDay

---
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ThisDayInfo.module.scss';
+interface Props {
+
+}
+
+export const ThisDayInfo = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
