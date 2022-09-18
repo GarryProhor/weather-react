413c9f25da9abc749c8df1d58d113d05365e839d
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-09-18 15:07:32 +0300

    Popup

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import {Home} from "./pages/Home/components/Home";
 import {MonthStatistic} from "./pages/MonthStatistic/MonthStatistic";
 import {Route, Routes} from "react-router";
 import {Header} from "./shared/Header/Header";
+import {Popup} from "./shared/Popup/Popup";
 
 
 function App() {
     return (
-        <div className="container">
-            <Header />
-            <Routes>
-                <Route path="/"  element={<Home/>}/>
-                <Route path='/month-statistic'  element={<MonthStatistic/>}/>
-            </Routes>
+        <div className='global-container'>
+            <Popup />
+            <div className="container">
+                <Header />
+                <Routes>
+                    <Route path="/"  element={<Home/>}/>
+                    <Route path='/month-statistic'  element={<MonthStatistic/>}/>
+                </Routes>
+            </div>
         </div>
     );
 }
